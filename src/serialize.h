52de0ca84415d414c9ae86f963559a2617b8a17a
THETCR <thetcr@live.nl>
2018-10-26 22:38:14 +0200

    [0.4] Merge

--- src/serialize.h
+++ src/serialize.h
@@ -163,6 +163,25 @@ inline float ser_uint32_to_float(uint32_t y)
     return tmp.x;
 }
 
+/**
+ * others derived from vector
+ */
+inline unsigned int GetSerializeSize(const CScript& v, int nType, int nVersion)
+{
+    return GetSerializeSize((const std::vector<unsigned char>&)v, nType, nVersion);
+}
+
+template <typename Stream>
+void Serialize(Stream& os, const CScript& v, int nType, int nVersion)
+{
+    Serialize(os, (const std::vector<unsigned char>&)v, nType, nVersion);
+}
+
+template <typename Stream>
+void Unserialize(Stream& is, CScript& v, int nType, int nVersion)
+{
+    Unserialize(is, (std::vector<unsigned char>&)v, nType, nVersion);
+}
 
 /////////////////////////////////////////////////////////////////
 //
