7f39896f5f64cbbfddaa94629e2301f3c0197b1a
THETCR <thetcr@live.nl>
2018-10-26 11:15:26 +0200

    [0.4] Merge

--- src/pos/kernel.h
+++ src/pos/kernel.h
@@ -7,38 +7,50 @@
 #define WISPR_POS_KERNEL_H
 
 #include <validation.h>
+#include <wallet/stakeinput.h>
+// To decrease granularity of timestamp
+// Supposed to be 2^n-1
+static const int STAKE_TIMESTAMP_MASK = 15;
+// MODIFIER_INTERVAL: time to elapse before new modifier is computed
+static const unsigned int MODIFIER_INTERVALV1 = 10 * 60;
+static const unsigned int MODIFIER_INTERVALV2 = 60;
+static const unsigned int MODIFIER_INTERVAL_TESTNETV1 = 10 * 60;
+static const unsigned int MODIFIER_INTERVAL_TESTNETV2 = 60;
+extern unsigned int nModifierInterval;
+extern unsigned int getIntervalVersion(bool fTestNet);
+extern unsigned int GetStakeMinAge();
 
+// MODIFIER_INTERVAL_RATIO:
+// ratio of group interval length between the last group and the first group
+static const int MODIFIER_INTERVAL_RATIO = 3;
 
 // Compute the hash modifier for proof-of-stake
-uint256 ComputeStakeModifierV2(const CBlockIndex *pindexPrev, const uint256 &kernel);
+bool GetKernelStakeModifier(uint256 hashBlockFrom, uint64_t& nStakeModifier, int& nStakeModifierHeight, int64_t& nStakeModifierTime, bool fPrintProofOfStake);
+uint256 ComputeStakeModifier(const CBlockIndex* pindexPrev, const uint256& kernel);
+bool ComputeNextStakeModifier(const CBlockIndex* pindexPrev, uint64_t& nStakeModifier, bool& fGeneratedStakeModifier);
+bool CheckStakeV2(const CDataStream& ssUniqueID, CAmount nValueIn, const uint64_t nStakeModifier, const uint256& bnTarget, unsigned int nTimeBlockFrom, unsigned int& nTimeTx, uint256& hashProofOfStake);
+bool CheckStakeV1(unsigned int nTxPrevTime, const COutPoint &prevout,
+                  unsigned int nTimeTx, uint256 &hashProofOfStake, int64_t nValueIn, CBlockIndex *pindexPrev,
+                  unsigned int nBits, bool fDebug);
+bool stakeTargetHit(uint256 hashProofOfStake, int64_t nValueIn, uint256 bnTargetPerCoinDay);
+bool stakeTargetHitOld(uint256 hashProofOfStake, uint256 bnTargetPerCoinDay);
+bool Stake(CStakeInput* stakeInput, unsigned int nBits, unsigned int nTimeBlockFrom, unsigned int& nTimeTx, uint256& hashProofOfStake);
 
-/**
- * Check whether stake kernel meets hash target
- * Sets hashProofOfStake on success return
- */
-bool CheckStakeKernelHash(const CBlockIndex *pindexPrev,
-    uint32_t nBits, uint32_t nBlockFromTime,
-    CAmount prevOutAmount, const COutPoint &prevout, uint32_t nTimeTx,
-    uint256 &hashProofOfStake, uint256 &targetProofOfStake,
-    bool fPrintProofOfStake=false);
+// Check kernel hash target and coinstake signature
+// Sets hashProofOfStake on success return
+bool CheckProofOfStake(const CBlock block, uint256& hashProofOfStake, std::unique_ptr<CStakeInput>& stake);
 
+// Check whether the coinstake timestamp meets protocol
+bool CheckCoinStakeTimestamp(int64_t nTimeBlock, int64_t nTimeTx);
 
-/**
- * Check kernel hash target and coinstake signature
- * Sets hashProofOfStake on success return
- */
-bool CheckProofOfStake(CValidationState &state, const CBlockIndex *pindexPrev, const CTransaction &tx, int64_t nTime, unsigned int nBits, uint256 &hashProofOfStake, uint256 &targetProofOfStake) EXCLUSIVE_LOCKS_REQUIRED(cs_main);
+// Get stake modifier checksum
+unsigned int GetStakeModifierChecksum(const CBlockIndex* pindex);
 
-/**
- * Check whether the coinstake timestamp meets protocol
- */
-bool CheckCoinStakeTimestamp(int nHeight, int64_t nTimeBlock);
+// Check stake modifier hard checkpoints
+bool CheckStakeModifierCheckpoints(int nHeight, unsigned int nStakeModifierChecksum);
 
-/**
- * Wrapper around CheckStakeKernelHash()
- * Also checks existence of kernel input and min age
- * Convenient for searching a kernel
- */
-bool CheckKernel(const CBlockIndex *pindexPrev, unsigned int nBits, int64_t nTime, const COutPoint &prevout, int64_t* pBlockTime = nullptr);
+// Get time weight using supplied timestamps
+int64_t GetWeight(int64_t nIntervalBeginning, int64_t nIntervalEnd);
+
 
 #endif // WISPR_POS_KERNEL_H
