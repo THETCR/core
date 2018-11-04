aef0c8dfcec16e90ff4f0cd4a134836b7e5934cf
THETCR <thetcr@live.nl>
2018-11-04 15:53:29 +0100

    [0.4] Merge

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -1,5 +1,5 @@
 PACKAGE=qt
-$(package)_version=5.9.7
+$(package)_version=5.9.6
 $(package)_download_path=https://download.qt.io/official_releases/qt/5.9/$($(package)_version)/submodules
 $(package)_suffix=opensource-src-$($(package)_version).tar.xz
 $(package)_file_name=qtbase-$($(package)_suffix)
