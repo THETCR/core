e436bb40f6bf2c715d11b17fa3ea1d5721a91c9f
THETCR <thetcr@live.nl>
2018-11-04 12:42:21 +0100

    [0.4] Merge

--- src/primitives/deterministicmint.cpp
+++ src/primitives/deterministicmint.cpp
@@ -2,9 +2,9 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <primitives/deterministicmint.h>
 #include <libzerocoin/Coin.h>
 #include <tinyformat.h>
-#include "deterministicmint.h"
 
 using namespace libzerocoin;
 
