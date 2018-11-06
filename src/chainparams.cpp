69b8be5d927813a311ec015d8fdd432ed81bf7f2
THETCR <thetcr@live.nl>
2018-11-06 11:35:40 +0100

    [0.4] Merge

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -570,6 +570,28 @@ public:
         nFulfilledRequestExpireTime = 60*60; // fulfilled requests expire in 1 hour
 
         strSporkAddress = "Xgtyuk76vhuFW2iT7UAiHgNdWXCf3J34wh";
+        //WISPR
+        /** Height or Time Based Activations **/
+        nLastPOWBlock = 450;
+        nNewProtocolStartHeight = 400000;
+        nNewProtocolStartTime = 1539963322; //Friday, October 19, 2018 3:35:22 PM
+        nZerocoinStartHeight = nNewProtocolStartHeight;
+        nZerocoinStartTime = nNewProtocolStartTime;
+        /** Zerocoin */
+        zerocoinModulus = "25195908475657893494027183240048398571429282126204032027777137836043662020707595556264018525880784"
+                          "4069182906412495150821892985591491761845028084891200728449926873928072877767359714183472702618963750149718246911"
+                          "6507761337985909570009733045974880842840179742910064245869181719511874612151517265463228221686998754918242243363"
+                          "7259085141865462043576798423387184774447920739934236584823824281198163815010674810451660377306056201619676256133"
+                          "8441436038339044149526344321901146575444541784240209246165157233507787077498171257724679629263863563732899121548"
+                          "31438167899885040445364023527381951378636564391212010397122822120720357";
+        nMaxZerocoinSpendsPerTransaction = 7; // Assume about 20kb each
+        nMinZerocoinMintFee = 1 * CENT; //high fee required for zerocoin mints
+        nMintRequiredConfirmations = 20; //the maximum amount of confirmations until accumulated in 19
+        nRequiredAccumulation = 1;
+        nDefaultSecurityLevel = 100; //full security level for accumulators
+        nZerocoinHeaderVersion = 8; //Block headers must be this version once zerocoin is active
+        nZerocoinRequiredStakeDepth = 200; //The required confirmations for a zwsp to be stakable
+
         checkpointData = {
             {
                 { 5000,     uint256S("0xe786020ab94bc5461a07d744f3631a811b4ebf424fceda12274f2321883713f4")},
