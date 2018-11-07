ef7942e01f55af2b8b80cd4a18b0d4a7f4bd8e33
THETCR <thetcr@live.nl>
2018-11-07 15:58:04 +0100

    [0.4] Merge

--- src/zerocoin/zwspchain.cpp
+++ src/zerocoin/zwspchain.cpp
@@ -2,6 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <consensus/validation.h>
 #include <zerocoin/zwspchain.h>
 #include <invalid.h>
 #include <chainparams.h>
