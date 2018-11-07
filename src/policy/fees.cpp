1ef40aa47bc010fe5503fcaf575b991b12aae4be
THETCR <thetcr@live.nl>
2018-11-08 00:33:58 +0100

    [0.4] Merge wallet

--- src/policy/fees.cpp
+++ src/policy/fees.cpp
@@ -563,6 +563,23 @@ CBlockPolicyEstimator::CBlockPolicyEstimator()
 CBlockPolicyEstimator::~CBlockPolicyEstimator()
 {
 }
+double CBlockPolicyEstimator::estimatePriority(int confTarget)
+{
+    return -1;
+}
+
+double CBlockPolicyEstimator::estimateSmartPriority(int confTarget, int *answerFoundAtTarget, const CTxMemPool& pool)
+{
+    if (answerFoundAtTarget)
+        *answerFoundAtTarget = confTarget;
+
+    // If mempool is limiting txs, no priority txs are allowed
+    CAmount minPoolFee = pool.GetMinFee(GetArg("-maxmempool", DEFAULT_MAX_MEMPOOL_SIZE) * 1000000).GetFeePerK();
+    if (minPoolFee > 0)
+        return INF_PRIORITY;
+
+    return -1;
+}
 
 void CBlockPolicyEstimator::processTransaction(const CTxMemPoolEntry& entry, bool validFeeEstimate)
 {
