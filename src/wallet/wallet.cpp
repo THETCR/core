f43e0db6f5c72f29b1996c386a98cc742ed540cb
THETCR <thetcr@live.nl>
2018-11-09 16:32:20 +0100

    [0.4] Merge wallet

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3287,9 +3287,8 @@ bool CWallet::CreateTransaction(const std::vector<CRecipient>& vecSend, CTransac
                     nFeeNeeded = std::max(nFeeNeeded, CTxLockRequest(txNew).GetMinFee());
                 }
                 if (coin_control.fOverrideFeeRate)
-//                    CFeeRate mFeeRate = *coin_control.m_feerate;
-
-                    nFeeNeeded = coin_control.nFeeRate.GetFee(nBytes);
+                    CFeeRate fR = *coin_control.m_feerate;
+                    nFeeNeeded = fR.GetFee(nBytes);
 
                 // If we made it here and we aren't even able to meet the relay fee on the next pass, give up
                 // because we must be at the maximum allowed fee.
