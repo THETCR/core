ac124b7b41de4af13b9c5ea7b9999160b5764e0a
THETCR <thetcr@live.nl>
2018-11-09 17:05:35 +0100

    [0.4] Merge wallet

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3232,7 +3232,7 @@ bool CWallet::CreateTransaction(const std::vector<CRecipient>& vecSend, CTransac
                     //a chance at a free transaction.
                     //But mempool inputs might still be in the mempool, so their age stays 0
 //                    const CWalletTx *pcoin = &walletEntry.second;
-                    CWalletTx &wTx = this.GetWalletTx(txin.prevout.hash);
+                    CWalletTx &wTx = this->GetWalletTx(txin.prevout.hash);
                     int age = wTx.GetDepthInMainChain();
                     assert(age >= 0);
                     if (age != 0)
