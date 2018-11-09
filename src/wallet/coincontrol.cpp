2b5cac73e55d58a9abd5dfc2a6b02d79f97ac59b
THETCR <thetcr@live.nl>
2018-11-09 10:23:17 +0100

    [0.4] Merge wallet

--- src/wallet/coincontrol.cpp
+++ src/wallet/coincontrol.cpp
@@ -19,7 +19,9 @@ void CCoinControl::SetNull()
     m_confirm_target.reset();
     m_signal_bip125_rbf.reset();
     m_fee_mode = FeeEstimateMode::UNSET;
-
+    fUseInstantSend = false;
+    fUsePrivateSend = true;
+    nMinimumTotalFee = 0;
     nCoinType = OUTPUT_STANDARD;
     fHaveAnonOutputs = false;
     m_extrafee = 0;
--- src/wallet/coincontrol.h
+++ src/wallet/coincontrol.h
@@ -48,6 +48,8 @@ public:
     //! Override automatic min/max checks on fee, m_feerate must be set if true
     bool fOverrideFeeRate;
 
+    //! Minimum absolute fee (not per kilobyte)
+    CAmount nMinimumTotalFee;
     //! Override the wallet's m_pay_tx_fee if set
     boost::optional<CFeeRate> m_feerate;
     //! Override the default confirmation target if set
