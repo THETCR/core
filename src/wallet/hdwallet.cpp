456fb08eee60b16ec00b67e91f1db656a19bf691
THETCR <thetcr@live.nl>
2018-11-11 12:25:53 +0100

    [0.4] Merge wallet

--- src/wallet/hdwallet.cpp
+++ src/wallet/hdwallet.cpp
@@ -13090,7 +13090,8 @@ bool CHDWallet::CreateZerocoinSpendTransaction(CAmount nValue, int nSecurityLeve
             }
 
             //turn the finalized transaction into a wallet transaction
-            *wtxNew = new CWalletTx(this, MakeTransactionRef(std::move(txNew)));
+            CWalletTx *wNew = new CWalletTx(this, MakeTransactionRef(std::move(txNew)));
+            wtxNew = &wNew;
 //            wtxNew = CWalletTx(this, txNew);
             wtxNew.fFromMe = true;
             wtxNew.fTimeReceivedIsTxTime = true;
