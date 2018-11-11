0f1272fdf25ba580d0ada6db30b67a6c566ddcd0
THETCR <thetcr@live.nl>
2018-11-11 23:23:40 +0100

    [0.4] Merge wallet

--- src/wallet/crypter.h
+++ src/wallet/crypter.h
@@ -11,6 +11,7 @@
 
 #include <atomic>
 
+class CHDWallet;
 const unsigned int WALLET_CRYPTO_KEY_SIZE = 32;
 const unsigned int WALLET_CRYPTO_SALT_SIZE = 8;
 const unsigned int WALLET_CRYPTO_IV_SIZE = 16;
