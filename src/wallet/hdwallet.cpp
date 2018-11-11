dc3ac71de053ade73e16532af98b4419e29a0f67
THETCR <thetcr@live.nl>
2018-11-11 12:31:29 +0100

    [0.4] Merge wallet

--- src/wallet/hdwallet.cpp
+++ src/wallet/hdwallet.cpp
@@ -12137,7 +12137,7 @@ bool CHDWallet::SelectStakeCoins(std::list<std::unique_ptr<CStakeInput> >& listI
             nAmountSelected += ref.vout[out.i].nValue;
 
             std::unique_ptr<CWspStake> input(new CWspStake());
-            CTransactionRef tRef = &out.tx;
+            CTransactionRef *tRef = out.tx;
             input->SetInput(tRef, out.i);
             listInputs.emplace_back(std::move(input));
         }
