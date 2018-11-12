4d2dfa09f28fb34b37c05b1006db4724ca81c843
THETCR <thetcr@live.nl>
2018-11-12 12:21:38 +0100

    [0.4] Merge wallet

--- src/script/sign.cpp
+++ src/script/sign.cpp
@@ -523,14 +523,15 @@ bool SignSignature(const CKeyStore &keystore, const CScript& fromPubKey, CMutabl
     return ProduceSignature(keystore, creator, fromPubKey, sigs);
 }
 
-bool SignSignature(const CKeyStore &keystore, const CTransaction& txFrom, CMutableTransaction& txTo, unsigned int nIn, int nHashType)
+bool SignSignature(const CKeyStore &keystore, const CTransaction& txFrom, CMutableTransaction& txTo, unsigned int nIn, const CAmount amount, int nHashType)
 {
     assert(nIn < txTo.vin.size());
     CTxIn& txin = txTo.vin[nIn];
     assert(txin.prevout.n < txFrom.vout.size());
     const CTxOut& txout = txFrom.vout[txin.prevout.n];
-
-    return SignSignature(keystore, txout.scriptPubKey, txTo, nIn, nHashType);
+    std::vector<uint8_t> vamount(8);
+    memcpy(vamount.data(), &amount, 8);
+    return SignSignature(keystore, txout.scriptPubKey, txTo, nIn, amount, nHashType);
 }
 
 
