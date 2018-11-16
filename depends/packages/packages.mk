625b5cff4650358112da1fad7c606d3ae81fe722
THETCR <thetcr@live.nl>
2018-11-16 14:07:14 +0100

    [0.4] Merge wallet

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,10 +1,11 @@
 packages:=boost openssl libevent zeromq gmp
-linux_packages_usb := libusb
 
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
 
 qt_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
+
+rapidcheck_packages = rapidcheck
 
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
@@ -12,8 +13,7 @@ qt_mingw32_packages=qt
 wallet_packages=bdb
 
 upnp_packages=miniupnpc
-usb_packages=hidapi protobuf
-rapidcheck_packages = rapidcheck
+usb_packages=libusb hidapi protobuf
 usb_native_packages=native_protobuf
 
 darwin_native_packages = native_biplist native_ds_store native_mac_alias
