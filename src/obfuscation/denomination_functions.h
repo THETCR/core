0864170abfa1112b9a66490ac61dccbc3eecbf0e
THETCR <thetcr@live.nl>
2018-11-16 22:40:07 +0100

    [0.4] Merge wallet

--- src/obfuscation/denomination_functions.h
+++ src/obfuscation/denomination_functions.h
@@ -2,7 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <reverse_iterate.h>
+#include <reverse_iterator.h>
 #include <util/system.h>
 #include <libzerocoin/Denominations.h>
 #include <primitives/zerocoin.h>
