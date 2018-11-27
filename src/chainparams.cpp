fd0d215d88911f10c6c9579b7a2828fc93681b11
THETCR <thetcr@live.nl>
2018-11-27 19:35:01 +0100

    [0.4] Merge

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -466,10 +466,10 @@ public:
         consensus.hashGenesisBlock = genesis.GetHash();
 
         printf("Main net block\n");
-        printf("Genesis hash = %s\n", consensus.hashGenesisBlock.ToString());
-        printf("Genesis merkle = %s\n", genesis.hashMerkleRoot.ToString());
-        printf("Genesis hash = %s\n", genesis.hashWitnessMerkleRoot.ToString());
-        printf("Genesis = %s\n", genesis.ToString());
+        printf("Genesis hash = %s\n", consensus.hashGenesisBlock.ToString().c_str());
+        printf("Genesis merkle = %s\n", genesis.hashMerkleRoot.ToString().c_str());
+        printf("Genesis hash = %s\n", genesis.hashWitnessMerkleRoot.ToString().c_str());
+        printf("Genesis = %s\n", genesis.ToString().c_str());
 
 //        assert(consensus.hashGenesisBlock == uint256S("0x0000ee0784c195317ac95623e22fddb8c7b8825dc3998e0bb924d66866eccf4c"));
 //        assert(genesis.hashMerkleRoot == uint256S("0xc95fb023cf4bc02ddfed1a59e2b2f53edd1a726683209e2780332edf554f1e3e"));
