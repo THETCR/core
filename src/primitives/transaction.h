096775189d906b2a0ec57da6c6b638bede2be21a
THETCR <thetcr@live.nl>
2018-12-16 18:14:25 +0100

    [0.4] CMAKE

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -931,6 +931,18 @@ public:
 
     bool IsCoinStake() const
     {
+        if (vin.empty())
+            return false;
+
+        // ppcoin: the coin stake transaction is marked with the first output empty
+        bool fAllowNull = vin[0].scriptSig.IsZerocoinSpend();
+        if (vin[0].prevout.IsNull() && !fAllowNull)
+            return false;
+
+        if (vin.size() > 0 && vout.size() >= 2 && vout[0].IsEmpty()){
+            return true;
+        }
+
         return GetType() == TXN_COINSTAKE
             && vin.size() > 0 && vpout.size() > 1
             && vpout[0]->nVersion == OUTPUT_DATA
