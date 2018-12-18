c09726125121f47ee2b841dd697c5ddeba53424c
THETCR <thetcr@live.nl>
2018-12-18 02:36:17 +0100

    [0.4] Remove boost

--- src/wispr-tx.cpp
+++ src/wispr-tx.cpp
@@ -26,7 +26,7 @@
 #include <memory>
 #include <stdio.h>
 
-#include <util/splitstring.h>
+#include <boost/algorithm/string.hpp>
 
 static bool fCreateBlank;
 static std::map<std::string,UniValue> registers;
