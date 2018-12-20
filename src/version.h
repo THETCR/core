b22812edb303f1c67ced81bf93d9a6905fe62e46
THETCR <thetcr@live.nl>
2018-12-20 17:20:02 +0100

    [0.4] Process message debug

--- src/version.h
+++ src/version.h
@@ -35,7 +35,7 @@ static const int BIP0031_VERSION = 60000;
 static const int NO_BLOOM_VERSION = 70011;
 
 //! "sendheaders" command and announcing blocks with headers starts with this version
-static const int SENDHEADERS_VERSION = 70915;
+static const int SENDHEADERS_VERSION = 70916;
 
 //! "feefilter" tells peers to filter invs to you by fee starts with this version
 static const int FEEFILTER_VERSION = 70915;
