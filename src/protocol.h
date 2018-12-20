06812ed32851b662b881102b27697a077a37c2d7
THETCR <thetcr@live.nl>
2018-12-20 18:29:15 +0100

    [0.4] Process message debug

--- src/protocol.h
+++ src/protocol.h
@@ -59,7 +59,8 @@ public:
     char pchMessageStart[MESSAGE_START_SIZE];
     char pchCommand[COMMAND_SIZE];
     unsigned int nMessageSize;
-    unsigned int pchChecksum[CHECKSUM_SIZE];
+    unsigned int pchChecksum;
+//    unsigned int pchChecksum[CHECKSUM_SIZE];
 };
 
 /**
