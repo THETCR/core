84d740ad4229a36a60b19cea1ac1a6f0e154acc8
THETCR <thetcr@live.nl>
2019-02-11 11:57:38 +0100

    [CORE] Fix backport bitcoin@fef1010199b70026fd6d56ebac5c277552757307 https://github.com/bitcoin/bitcoin/commit/fef1010199b70026fd6d56ebac5c277552757307

--- src/test/test_wispr_main.cpp
+++ src/test/test_wispr_main.cpp
@@ -5,19 +5,11 @@
 #define BOOST_TEST_MODULE Wispr Test Suite
 
 #include "net.h"
-#include "ui_interface.h"
-#ifdef ENABLE_WALLET
-#include "db.h"
-#include "wallet.h"
-#include "walletdb.h"
-#include "accumulators.h"
-#endif
 
 #include <memory>
 
 #include <boost/test/unit_test.hpp>
 
-CClientUIInterface uiInterface;
 std::unique_ptr<CConnman> g_connman;
 
 [[noreturn]] void Shutdown(void* parg)
