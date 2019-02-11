e6dd2dfe9e136e8f3e2110ca8dcbde0c8d6961ef
THETCR <thetcr@live.nl>
2019-02-11 11:10:33 +0100

    [CORE] Fix backport bitcoin@fef1010199b70026fd6d56ebac5c277552757307 https://github.com/bitcoin/bitcoin/commit/fef1010199b70026fd6d56ebac5c277552757307

--- src/test/test_wispr_main.cpp
+++ src/test/test_wispr_main.cpp
@@ -2,17 +2,15 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#define BOOST_TEST_MODULE Bitcoin Test Suite
+#define BOOST_TEST_MODULE Wispr Test Suite
 
-#include <banman.h>
-#include <net.h>
+#include "net.h"
 
 #include <memory>
 
 #include <boost/test/unit_test.hpp>
 
 std::unique_ptr<CConnman> g_connman;
-std::unique_ptr<BanMan> g_banman;
 
 [[noreturn]] void Shutdown(void* parg)
 {
