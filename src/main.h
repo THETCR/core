6f92efe6be5d3a014d3a8710ae8ff4558b8c8420
THETCR <thetcr@live.nl>
2019-02-13 15:05:25 +0100

    [CORE] Backport e736772c56a883e2649cc8534dd7857a0718ec56 net_processing

--- src/main.h
+++ src/main.h
@@ -85,6 +85,7 @@ static const unsigned int BLOCK_DOWNLOAD_WINDOW = 1024;
 static const unsigned int DATABASE_WRITE_INTERVAL = 3600;
 /** Maximum length of reject messages. */
 static const unsigned int MAX_REJECT_MESSAGE_LENGTH = 111;
+static const unsigned int DEFAULT_BANSCORE_THRESHOLD = 100;
 
 /** Maximum number of headers to announce when relaying blocks with headers message.*/
 static const unsigned int MAX_BLOCKS_TO_ANNOUNCE = 8;
