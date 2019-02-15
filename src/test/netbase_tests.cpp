3c469bb968da109faa98b2f1839b2be5317d573c
THETCR <thetcr@live.nl>
2019-02-15 22:38:50 +0100

    [CORE] Backport fix

--- src/test/netbase_tests.cpp
+++ src/test/netbase_tests.cpp
@@ -4,7 +4,9 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "netbase.h"
+#include <netbase.h>
+#include <test/test_wispr.h>
+#include <utilstrencodings.h>
 
 #include <string>
 
