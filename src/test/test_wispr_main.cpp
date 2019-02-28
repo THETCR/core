56f395c64819168da61d8567ac02fdd127742179
THETCR <thetcr@live.nl>
2019-02-28 20:03:30 +0100

    [CORE] Debug

--- src/test/test_wispr_main.cpp
+++ src/test/test_wispr_main.cpp
@@ -11,17 +11,17 @@
 
 #include <boost/test/unit_test.hpp>
 
-//std::unique_ptr<CConnman> g_connman;
+std::unique_ptr<CConnman> g_connman;
 //BOOST_GLOBAL_FIXTURE(TestingSetup);
 
-//[[noreturn]] void Shutdown(void* parg) {
-//  std::exit(EXIT_SUCCESS);
-//}
-//
-//[[noreturn]] void StartShutdown() {
-//  std::exit(EXIT_SUCCESS);
-//}
-//
-//bool ShutdownRequested() {
-//  return false;
-//}
+[[noreturn]] void Shutdown(void* parg) {
+  std::exit(EXIT_SUCCESS);
+}
+
+[[noreturn]] void StartShutdown() {
+  std::exit(EXIT_SUCCESS);
+}
+
+bool ShutdownRequested() {
+  return false;
+}
