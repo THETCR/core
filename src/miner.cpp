560e3328a3e761a56d2399428a59ac757bd825ba
THETCR <thetcr@live.nl>
2019-02-28 18:07:55 +0100

    [CORE] Backport unique pointers

--- src/miner.cpp
+++ src/miner.cpp
@@ -189,7 +189,7 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
 
         CBlockIndex* pindexPrev = chainActive.Tip();
         const int nHeight = pindexPrev->nHeight + 1;
-        CCoinsViewCache view(pcoinsTip);
+        CCoinsViewCache view(pcoinsTip.get());
 
         // Priority order to process transactions
         std::list<COrphan> vOrphan; // list memory doesn't move
