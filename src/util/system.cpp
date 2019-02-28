052597a416688f8976b5179860dcb1251c0ea525
THETCR <thetcr@live.nl>
2019-02-28 16:12:23 +0100

    [CORE] Backport init functions and wisprd

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -245,7 +245,7 @@ public:
       std::pair<bool,std::string> found_result(false, std::string());
 
       // We pass "true" to GetArgHelper in order to return the last
-      // argument value seen from the command line (so "bitcoind -foo=bar
+      // argument value seen from the command line (so "wisprd -foo=bar
       // -foo=baz" gives GetArg(am,"foo")=={true,"baz"}
       found_result = GetArgHelper(am.m_override_args, arg, true);
       if (found_result.first) {
@@ -691,7 +691,7 @@ static std::string FormatException(const std::exception* pex, const char* pszThr
     char pszModule[MAX_PATH] = "";
     GetModuleFileNameA(nullptr, pszModule, sizeof(pszModule));
 #else
-    const char* pszModule = "bitcoin";
+    const char* pszModule = "wispr";
 #endif
     if (pex)
         return strprintf(
@@ -710,13 +710,13 @@ void PrintExceptionContinue(const std::exception* pex, const char* pszThread)
 
 fs::path GetDefaultDataDir()
 {
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Bitcoin
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Bitcoin
-    // Mac: ~/Library/Application Support/Bitcoin
-    // Unix: ~/.bitcoin
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Wispr
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Wispr
+    // Mac: ~/Library/Application Support/Wispr
+    // Unix: ~/.wispr
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Wispr";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -726,10 +726,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/Wispr";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".wispr";
 #endif
 #endif
 }
@@ -1245,9 +1245,9 @@ std::string CopyrightHolders(const std::string& strPrefix)
 {
     std::string strCopyrightHolders = strPrefix + strprintf(_(COPYRIGHT_HOLDERS), _(COPYRIGHT_HOLDERS_SUBSTITUTION));
 
-    // Check for untranslated substitution to make sure Bitcoin Core copyright is not removed by accident
-    if (strprintf(COPYRIGHT_HOLDERS, COPYRIGHT_HOLDERS_SUBSTITUTION).find("Bitcoin Core") == std::string::npos) {
-        strCopyrightHolders += "\n" + strPrefix + "The Bitcoin Core developers";
+    // Check for untranslated substitution to make sure Wispr Core copyright is not removed by accident
+    if (strprintf(COPYRIGHT_HOLDERS, COPYRIGHT_HOLDERS_SUBSTITUTION).find("Wispr Core") == std::string::npos) {
+        strCopyrightHolders += "\n" + strPrefix + "The Wispr Core developers";
     }
     return strCopyrightHolders;
 }
