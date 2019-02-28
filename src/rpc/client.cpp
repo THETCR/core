a468a2642a0071d0851211b3936121bde92ee098
THETCR <thetcr@live.nl>
2019-02-28 21:56:16 +0100

    [CORE] Debug remove pre and post command

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -5,151 +5,157 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "rpc/client.h"
-
-#include "rpc/protocol.h"
+#include <rpc/client.h>
+#include <rpc/protocol.h>
 #include <util/system.h>
 
 #include <set>
 #include <stdint.h>
 
-#include <boost/algorithm/string/case_conv.hpp> // for to_lower()
-#include <univalue.h>
-
-using namespace std;
 
 class CRPCConvertParam
 {
 public:
     std::string methodName; //! method whose params want conversion
     int paramIdx;           //! 0-based idx of param to convert
+    std::string paramName;  //!< parameter name
 };
-// ***TODO***
+
+
+// clang-format off
+/**
+ * Specify a (method, idx, name) here if the argument is a non-string RPC
+ * argument and needs to be converted from JSON.
+ *
+ * @note Parameter indexes start from 0.
+ */
 static const CRPCConvertParam vRPCConvertParams[] =
     {
-        {"stop", 0},
-        {"setmocktime", 0},
-        {"getaddednodeinfo", 0},
-        {"setgenerate", 0},
-        {"setgenerate", 1},
-        {"getnetworkhashps", 0},
-        {"getnetworkhashps", 1},
-        {"sendtoaddress", 1},
-        {"sendtoaddressix", 1},
-        {"settxfee", 0},
-        {"getreceivedbyaddress", 1},
-        {"getreceivedbyaccount", 1},
-        {"listreceivedbyaddress", 0},
-        {"listreceivedbyaddress", 1},
-        {"listreceivedbyaddress", 2},
-        {"listreceivedbyaccount", 0},
-        {"listreceivedbyaccount", 1},
-        {"listreceivedbyaccount", 2},
-        {"getbalance", 1},
-        {"getbalance", 2},
-        {"getblockhash", 0},
-        {"move", 2},
-        {"move", 3},
-        {"sendfrom", 2},
-        {"sendfrom", 3},
-        {"listtransactions", 1},
-        {"listtransactions", 2},
-        {"listtransactions", 3},
-        {"listaccounts", 0},
-        {"listaccounts", 1},
-        {"walletpassphrase", 1},
-        {"walletpassphrase", 2},
-        {"getblocktemplate", 0},
-        {"listsinceblock", 1},
-        {"listsinceblock", 2},
-        {"sendmany", 1},
-        {"sendmany", 2},
-        {"addmultisigaddress", 0},
-        {"addmultisigaddress", 1},
-        {"createmultisig", 0},
-        {"createmultisig", 1},
-        {"listunspent", 0},
-        {"listunspent", 1},
-        {"listunspent", 2},
-        {"listunspent", 3},
-        {"getblock", 1},
-        {"getblockheader", 1},
-        {"gettransaction", 1},
-        {"getrawtransaction", 1},
-        {"createrawtransaction", 0},
-        {"createrawtransaction", 1},
-        {"signrawtransaction", 1},
-        {"signrawtransaction", 2},
-        {"sendrawtransaction", 1},
-        {"sendrawtransaction", 2},
-        {"gettxout", 1},
-        {"gettxout", 2},
-        {"lockunspent", 0},
-        {"lockunspent", 1},
-        {"importprivkey", 2},
-        {"importaddress", 2},
-        {"verifychain", 0},
-        {"verifychain", 1},
-        {"keypoolrefill", 0},
-        {"getrawmempool", 0},
-        {"estimatefee", 0},
-        {"estimatepriority", 0},
-        {"prioritisetransaction", 1},
-        {"prioritisetransaction", 2},
-        {"setban", 2},
-        {"setban", 3},
-        {"spork", 1},
-        {"mnbudget", 3},
-        {"mnbudget", 4},
-        {"mnbudget", 6},
-        {"mnbudget", 8},
-        {"preparebudget", 2},
-        {"preparebudget", 3},
-        {"preparebudget", 5},
-        {"submitbudget", 2},
-        {"submitbudget", 3},
-        {"submitbudget", 5},
-        {"submitbudget", 7},
+    { "stop", 0, "wait" },
+    { "setmocktime", 0, "timestamp" },
+    {"getaddednodeinfo", 0, "dns"},
+    {"setgenerate", 0, "generate"},
+    {"setgenerate", 1, "genproclimit"},
+    { "getnetworkhashps", 0, "nblocks" },
+    { "getnetworkhashps", 1, "height" },
+    { "sendtoaddress", 1, "amount" },
+    {"sendtoaddressix", 1, "amount"},
+    { "settxfee", 0, "amount" },
+    { "getreceivedbyaddress", 1, "minconf" },
+    { "getreceivedbyaccount", 1, "minconf" },
+    { "listreceivedbyaddress", 0, "minconf" },
+    { "listreceivedbyaddress", 1, "addlockconf" },
+    { "listreceivedbyaddress", 2, "include_empty" },
+    { "listreceivedbyaccount", 0, "minconf" },
+    { "listreceivedbyaccount", 1, "addlockconf" },
+    { "listreceivedbyaccount", 2, "include_empty" },
+    { "getbalance", 1, "minconf" },
+    { "getbalance", 2, "addlockconf" },
+    { "getblockhash", 0, "height" },
+    { "move", 2, "amount" },
+    { "move", 3, "minconf" },
+    { "sendfrom", 2, "amount" },
+    { "sendfrom", 3, "minconf" },
+    { "listtransactions", 1, "count" },
+    { "listtransactions", 2, "skip" },
+    { "listtransactions", 3, "include_watchonly" },
+    { "listaccounts", 0, "minconf" },
+    { "listaccounts", 1, "addlockconf" },
+    { "walletpassphrase", 1, "timeout" },
+    { "walletpassphrase", 2, "mixingonly" },
+    { "getblocktemplate", 0, "template_request" },
+    { "listsinceblock", 1, "target_confirmations" },
+    { "listsinceblock", 2, "include_watchonly" },
+    { "sendmany", 1, "amounts" },
+    { "sendmany", 2, "minconf" },
+    { "addmultisigaddress", 0, "nrequired" },
+    { "addmultisigaddress", 1, "keys" },
+    { "createmultisig", 0, "nrequired" },
+    { "createmultisig", 1, "keys" },
+    { "listunspent", 0, "minconf" },
+    { "listunspent", 1, "maxconf" },
+    { "listunspent", 2, "addresses" },
+    { "listunspent", 3, "include_unsafe" },
+    { "getblock", 1, "verbosity" },
+    { "getblockheader", 1, "verbose" },
+    { "gettransaction", 1, "include_watchonly" },
+    { "getrawtransaction", 1, "verbose" },
+    { "createrawtransaction", 0, "inputs" },
+    { "createrawtransaction", 1, "outputs" },
+    { "createrawtransaction", 2, "locktime" },
+    { "signrawtransaction", 1, "prevtxs" },
+    { "signrawtransaction", 2, "privkeys" },
+    { "sendrawtransaction", 1, "allowhighfees" },
+    { "sendrawtransaction", 2, "instantsend" },
+    { "sendrawtransaction", 3, "bypasslimits" },
+    { "gettxout", 1, "n" },
+    { "gettxout", 2, "include_mempool" },
+    { "lockunspent", 0, "unlock" },
+    { "lockunspent", 1, "transactions" },
+    { "importprivkey", 2, "rescan" },
+    { "importaddress", 2, "rescan" },
+    { "verifychain", 0, "checklevel" },
+    { "verifychain", 1, "nblocks" },
+    { "keypoolrefill", 0, "newsize" },
+    { "getrawmempool", 0, "verbose" },
+    { "estimatefee", 0, "nblocks" },
+    { "estimatepriority", 0, "nblocks" },
+    { "prioritisetransaction", 1, "priority_delta" },
+    { "prioritisetransaction", 2, "fee_delta" },
+    { "setban", 2, "bantime" },
+    { "setban", 3, "absolute" },
+    { "spork", 1, "value" },
+        {"mnbudget", 3, ""},
+        {"mnbudget", 4, ""},
+        {"mnbudget", 6, ""},
+        {"mnbudget", 8, ""},
+        {"preparebudget", 2, "payment-count"},
+        {"preparebudget", 3, "block-start"},
+        {"preparebudget", 5, "monthly-payment"},
+        {"submitbudget", 4, "payment-count"},
+        {"submitbudget", 5, "block-start"},
+        {"submitbudget", 7, "monthly-payment"},
         // disabled until removal of the legacy 'masternode' command
         //{"startmasternode", 1},
-        {"mnvoteraw", 1},
-        {"mnvoteraw", 4},
-        {"reservebalance", 0},
-        {"reservebalance", 1},
-        {"setstakesplitthreshold", 0},
-        {"autocombinerewards", 0},
-        {"autocombinerewards", 1},
-        {"getzerocoinbalance", 0},
-        {"listmintedzerocoins", 0},
-        {"listmintedzerocoins", 1},
-        {"listspentzerocoins", 0},
-        {"listzerocoinamounts", 0},
-        {"mintzerocoin", 0},
-        {"mintzerocoin", 1},
-        {"spendzerocoin", 0},
-        {"spendzerocoin", 1},
-        {"spendzerocoin", 2},
-        {"spendzerocoin", 3},
-        {"spendzerocoinmints", 0},
-        {"importzerocoins", 0},
-        {"exportzerocoins", 0},
-        {"exportzerocoins", 1},
-        {"resetmintzerocoin", 0},
-        {"getspentzerocoinamount", 1},
-        {"generatemintlist", 0},
-        {"generatemintlist", 1},
-        {"searchdzwsp", 0},
-        {"searchdzwsp", 1},
-        {"searchdzwsp", 2},
-        {"getaccumulatorvalues", 0},
-        {"getaccumulatorwitness",2},
-        {"getmintsvalues", 2},
-        {"enableautomintaddress", 0},
-        {"getmintsinblocks", 0},
-        {"getmintsinblocks", 1},
-        {"getmintsinblocks", 2},
-        {"getfeeinfo", 0}
+        {"mnvoteraw", 1, ""},
+        {"mnvoteraw", 4, ""},
+        {"reservebalance", 0, "reserve"},
+        {"reservebalance", 1, "amount"},
+        {"setstakesplitthreshold", 0, "value"},
+        {"autocombinerewards", 0, "enable"},
+        {"autocombinerewards", 1, "threshold"},
+        {"getzerocoinbalance", 0, ""},
+        {"listmintedzerocoins", 0, "fVerbose"},
+        {"listmintedzerocoins", 1, "fMatureOnly"},
+        {"listspentzerocoins", 0, ""},
+        {"listzerocoinamounts", 0, ""},
+        {"mintzerocoin", 0, "amount"},
+        {"mintzerocoin", 1, "utxos"},
+        {"spendzerocoin", 0, "amount"},
+        {"spendzerocoin", 1, "mintchange"},
+        {"spendzerocoin", 2, "minimizechange"},
+        {"spendzerocoin", 3, "securitylevel"},
+        {"spendzerocoinmints", 0, "mints_list"},
+        {"importzerocoins", 0, "importdata"},
+        {"exportzerocoins", 0, "include_spent"},
+        {"exportzerocoins", 1, "denomination"},
+        {"resetmintzerocoin", 0, "fullscan"},
+        {"getspentzerocoinamount", 1, "index"},
+        {"generatemintlist", 0, "count"},
+        {"generatemintlist", 1, "range"},
+        {"searchdzwsp", 0, "count"},
+        {"searchdzwsp", 1, "range"},
+        {"searchdzwsp", 2, "threads"},
+        {"getaccumulatorvalues", 0, "height"},
+        {"getaccumulatorwitness",2, "coinDenomination"},
+        {"getmintsvalues", 2, ""},
+        {"enableautomintaddress", 0, "enable"},
+        {"getmintsinblocks", 0, "height"},
+        {"getmintsinblocks", 1, "range"},
+        {"getmintsinblocks", 2, "coinDenomination"},
+        {"getfeeinfo", 0, "blocks"}
     };
+// clang-format on
 
 class CRPCConvertTable
 {
@@ -171,11 +177,13 @@ public:
 CRPCConvertTable::CRPCConvertTable()
 {
     const unsigned int n_elem =
-        (sizeof(vRPCConvertParams) / sizeof(vRPCConvertParams[0]));
+            (sizeof(vRPCConvertParams) / sizeof(vRPCConvertParams[0]));
 
     for (unsigned int i = 0; i < n_elem; i++) {
         members.insert(std::make_pair(vRPCConvertParams[i].methodName,
-            vRPCConvertParams[i].paramIdx));
+                                      vRPCConvertParams[i].paramIdx));
+        membersByName.insert(std::make_pair(vRPCConvertParams[i].methodName,
+                                            vRPCConvertParams[i].paramName));
     }
 }
 
@@ -189,11 +197,10 @@ UniValue ParseNonRFCJSONValue(const std::string& strVal)
     UniValue jVal;
     if (!jVal.read(std::string("[")+strVal+std::string("]")) ||
         !jVal.isArray() || jVal.size()!=1)
-        throw runtime_error(string("Error parsing JSON:")+strVal);
+        throw std::runtime_error(std::string("Error parsing JSON:")+strVal);
     return jVal[0];
 }
 
-/** Convert std::strings to command-specific RPC representation */
 UniValue RPCConvertValues(const std::string &strMethod, const std::vector<std::string> &strParams)
 {
     UniValue params(UniValue::VARR);
@@ -202,10 +209,10 @@ UniValue RPCConvertValues(const std::string &strMethod, const std::vector<std::s
         const std::string& strVal = strParams[idx];
 
         if (!rpcCvtTable.convert(strMethod, idx)) {
-            // insert std::string value directly
+            // insert string value directly
             params.push_back(strVal);
         } else {
-            // parse std::string as JSON, insert bool/number/object/etc. value
+            // parse string as JSON, insert bool/number/object/etc. value
             params.push_back(ParseNonRFCJSONValue(strVal));
         }
     }
