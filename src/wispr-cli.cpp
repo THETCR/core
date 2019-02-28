fe74c6aee3d0b4e4607e90829f4ecc8127bc346c
THETCR <thetcr@live.nl>
2019-02-28 22:20:46 +0100

    [CORE] Revert

--- src/wispr-cli.cpp
+++ src/wispr-cli.cpp
@@ -527,7 +527,6 @@ static int CommandLineRPC(int argc, char *argv[])
 
 int main(int argc, char* argv[])
 {
-    std::cout << "wispr-cli main\n";
 #ifdef WIN32
     util::WinCmdLineArgs winArgs;
     std::tie(argc, argv) = winArgs.get();
