e82a6b42bd20a8c0876b342dd6886f68aa4b4a17
THETCR <thetcr@live.nl>
2019-03-03 21:07:21 +0100

    [CORE] debug

--- src/test/sighash_tests.cpp
+++ src/test/sighash_tests.cpp
@@ -179,7 +179,7 @@ BOOST_AUTO_TEST_CASE(sighash_from_data)
         std::string raw_tx, raw_script, sigHashHex;
         int nIn, nHashType;
         uint256 sh;
-        CTransaction tx;
+        CTransactionRef tx;
         CScript scriptCode = CScript();
 
         try {
@@ -193,7 +193,7 @@ BOOST_AUTO_TEST_CASE(sighash_from_data)
             stream >> tx;
 
             CValidationState state;
-            BOOST_CHECK_MESSAGE(CheckTransaction(tx, false, false, state), strTest);
+            BOOST_CHECK_MESSAGE(CheckTransaction(*tx, false, false, state), strTest);
             BOOST_CHECK(state.IsValid());
 
             std::vector<unsigned char> raw = ParseHex(raw_script);
