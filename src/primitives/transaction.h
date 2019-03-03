8b25fb473b96d5b5487ffd4db1fff83ec6721093
THETCR <thetcr@live.nl>
2019-03-03 21:27:03 +0100

    [CORE] debug

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -304,29 +304,34 @@ public:
 
     CTransaction& operator=(const CTransaction& tx);
 
-    template <typename Stream>
-    inline void Serialize(Stream& s) const {
-        int32_t n32bitVersion = this->nVersion;
-        s << n32bitVersion;
-        if(n32bitVersion == 1){
-            s << nTime;
+//    template <typename Stream>
+//    inline void Serialize(Stream& s) const {
+//        int32_t n32bitVersion = this->nVersion;
+//        s << n32bitVersion;
+//        if(n32bitVersion == 1){
+//            s << nTime;
+//        }
+//        s << vin;
+//        s << vout;
+//        s << nLockTime;
+//    }
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action) {
+        READWRITE(*const_cast<int32_t*>(&this->nVersion));
+        int32_t _nVersion = this->nVersion;
+        if(_nVersion == 1){
+            READWRITE(*const_cast<unsigned int *>(&nTime));
         }
-        s << vin;
-        s << vout;
-        s << nLockTime;
+        READWRITE(*const_cast<std::vector<CTxIn>*>(&vin));
+        READWRITE(*const_cast<std::vector<CTxOut>*>(&vout));
+        READWRITE(*const_cast<uint32_t*>(&nLockTime));
+        if (ser_action.ForRead())
+            UpdateHash();
     }
 
-    template <typename Stream>
-    inline void Unserialize(Stream& s) {
-        s >> nVersion;
-        int32_t n32bitVersion = this->nVersion;
-        if(n32bitVersion == 1){
-            s >> nTime;
-        }
-        s >> vin;
-        s >> vout;
-        s >> nLockTime;
-    }
 
     /** This deserializing constructor is provided instead of an Unserialize method.
      *  Unserialize is not possible, since it would require overwriting const fields. */
