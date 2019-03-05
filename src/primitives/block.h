8be030bee7a8b873b274d344ff0f63922bcd015a
THETCR <thetcr@live.nl>
2019-03-05 08:51:54 +0100

    [CORE] debug

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -94,7 +94,7 @@ public:
     ADD_SERIALIZE_METHODS;
     template <typename Stream, typename Operation>
     inline void SerializationOp(Stream& s, Operation ser_action) {
-        READWRITE(*(CBlockHeader*)this);
+        READWRITEAS(CBlockHeader, *this);
         READWRITE(vtx);
     }
 };
