ebf4c07bde2e8ccd447c7622bea51ca7320c0507
TCR <thetcr@live.nl>
2019-03-25 16:46:03 +0100

    Backport fixes

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2135,7 +2135,7 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
             ptx = mempool.get(hash);
 //            bool fInMemPool = mempool.lookup(hash, tx);
             if (ptx == nullptr) continue; // another thread removed since queryHashes, maybe...
-            tx = CMutableTransaction(*ptx);
+            tx = *ptx;
             if ((pfrom->pfilter && pfrom->pfilter->IsRelevantAndUpdate(tx)) ||
                 (!pfrom->pfilter))
                 vInv.push_back(inv);
