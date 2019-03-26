699783147dd392b232d1e91f892aa38b4e6f98e8
TCR <thetcr@live.nl>
2019-03-26 03:59:22 +0100

    Backport rename qt header

--- src/policy/policy.cpp
+++ src/policy/policy.cpp
@@ -140,7 +140,7 @@ bool IsStandardTx(const CTransaction& tx, std::string& reason)
         else if ((whichType == TX_MULTISIG) && (!fIsBareMultisigStd)) {
             reason = "bare-multisig";
             return false;
-        } else if (IsDust(txout, ::dustRelayFee)) {
+        } else if (IsDust(txout, ::minRelayTxFee)) {
             reason = "dust";
             return false;
         }
