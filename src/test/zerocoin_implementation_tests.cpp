b498f5ece4805ef09a73945b35c854c6c856cf5c
TCR <thetcr@live.nl>
2019-04-01 21:22:48 +0200

    [CORE] Backport PIVX changes.

--- src/test/zerocoin_implementation_tests.cpp
+++ src/test/zerocoin_implementation_tests.cpp
@@ -2,7 +2,6 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <test/test_wispr.h>
 #include <wallet/test/wallet_test_fixture.h>
 
 #include <core_io.h>
