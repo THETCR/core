b346fc92dedb0d5b450c8440b23d049ccd000b8e
TCR <thetcr@live.nl>
2019-04-01 18:46:38 +0200

    [CORE] Backport PIVX changes.

--- src/zpiv/mintpool.cpp
+++ src/zpiv/mintpool.cpp
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "mintpool.h"
-#include "util.h"
+#include <zpiv/mintpool.h>
+#include <util/system.h>
 
 using namespace std;
 
