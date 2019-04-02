22a0d926b2155155fd908ee29e8a2adf0dbe405c
TCR <thetcr@live.nl>
2019-04-03 00:18:13 +0200

    [CORE] Backport fixes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -463,11 +463,12 @@ public:
      * Allows modifying the Version Bits regtest parameters.
      */
     void UpdateVersionBitsParameters(Consensus::DeploymentPos d, int64_t nStartTime, int64_t nTimeout)
-    {
+    {   std::cout << "UpdateVersionBitsParameters\n";
         consensus.vDeployments[d].nStartTime = nStartTime;
         consensus.vDeployments[d].nTimeout = nTimeout;
     }
     void UpdateVersionBitsParametersFromArgs(const ArgsManager& args);
+    std::cout << "Regtest constructor done\n";
 };
 
 /**
