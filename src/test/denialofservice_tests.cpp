5e92c4255c883f403ff3a44cd373dd6006c01052
TCR <thetcr@live.nl>
2019-04-02 03:09:27 +0200

    [CORE] Backport fixes

--- src/test/denialofservice_tests.cpp
+++ src/test/denialofservice_tests.cpp
@@ -39,7 +39,8 @@ struct CConnmanTest : public CConnman {
 };
 
 // Tests these internal-to-net_processing.cpp methods:
-extern bool AddOrphanTx(const CTransactionRef& tx, NodeId peer);
+//extern bool AddOrphanTx(const CTransactionRef& tx, NodeId peer);
+extern bool AddOrphanTx(const CTransaction& tx, NodeId peer);
 extern void EraseOrphansFor(NodeId peer);
 extern unsigned int LimitOrphanTxSize(unsigned int nMaxOrphans);
 //extern void Misbehaving(NodeId nodeid, int howmuch, const std::string& message="");
@@ -383,7 +384,7 @@ BOOST_AUTO_TEST_CASE(DoS_mapOrphans)
         tx.vout[0].nValue = 1*CENT;
         tx.vout[0].scriptPubKey = GetScriptForDestination(key.GetPubKey().GetID());
 
-        AddOrphanTx(MakeTransactionRef(tx), i);
+        AddOrphanTx(CTransaction(tx), i);
     }
 
     // ... and 50 that depend on other orphans:
@@ -400,7 +401,7 @@ BOOST_AUTO_TEST_CASE(DoS_mapOrphans)
         tx.vout[0].scriptPubKey = GetScriptForDestination(key.GetPubKey().GetID());
         BOOST_CHECK(SignSignature(keystore, *txPrev, tx, 0, SIGHASH_ALL));
 
-        AddOrphanTx(MakeTransactionRef(tx), i);
+        AddOrphanTx(CTransaction(tx), i);
     }
 
     // This really-big orphan should be ignored:
@@ -424,7 +425,7 @@ BOOST_AUTO_TEST_CASE(DoS_mapOrphans)
         for (unsigned int j = 1; j < tx.vin.size(); j++)
             tx.vin[j].scriptSig = tx.vin[0].scriptSig;
 
-        BOOST_CHECK(!AddOrphanTx(MakeTransactionRef(tx), i));
+        BOOST_CHECK(!AddOrphanTx(CTransaction(tx), i));
     }
 
     LOCK2(cs_main, g_cs_orphans);
