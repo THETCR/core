e3d6f008cc0810f8a7855786d993fd1c7c210bc8
TCR <thetcr@live.nl>
2019-04-02 19:19:36 +0200

    [CORE] return if no wallet is detected

--- src/test/test_wispr.cpp
+++ src/test/test_wispr.cpp
@@ -35,6 +35,7 @@ std::ostream& operator<<(std::ostream& os, const uint256& num)
 BasicTestingSetup::BasicTestingSetup(const std::string& chainName)
     : m_path_root(fs::temp_directory_path() / "test_wispr" / strprintf("%lu_%i", (unsigned long)GetTime(), (int)(InsecureRandRange(1 << 30))))
 {
+    std::cout << "SHA256AutoDetect\n";
     SHA256AutoDetect();
     ECC_Start();
     SetupEnvironment();
@@ -47,6 +48,7 @@ BasicTestingSetup::BasicTestingSetup(const std::string& chainName)
     gArgs.ForceSetArg("-vbparams", strprintf("segwit:0:%d", (int64_t)Consensus::BIP9Deployment::NO_TIMEOUT));
     SelectParams(chainName);
     noui_connect();
+    std::cout << "BasicTestingSetup finished\n";
 }
 
 BasicTestingSetup::~BasicTestingSetup()
@@ -78,30 +80,37 @@ TestingSetup::TestingSetup(const std::string& chainName) : BasicTestingSetup(cha
 
     // We have to run a scheduler thread to prevent ActivateBestChain
     // from blocking due to queue overrun.
+    std::cout << "serviceQueue\n";
     threadGroup.create_thread(std::bind(&CScheduler::serviceQueue, &scheduler));
     GetMainSignals().RegisterBackgroundSignalScheduler(scheduler);
 
+    std::cout << "mempool sanity\n";
     mempool.setSanityCheck(1.0);
     pblocktree.reset(new CBlockTreeDB(1 << 20, true));
     pcoinsdbview.reset(new CCoinsViewDB(1 << 23, true));
     pcoinsTip.reset(new CCoinsViewCache(pcoinsdbview.get()));
     zerocoinDB.reset(new CZerocoinDB(0, false, fReindex));
     pSporkDB.reset(new CSporkDB(0, false, false));
+    std::cout << "LoadGenesis\n";
     if (!LoadGenesisBlock(chainparams)) {
         throw std::runtime_error("LoadGenesisBlock failed.");
     }
 
     CValidationState state;
+    std::cout << "ActivateBestChain\n";
     if (!ActivateBestChain(state, chainparams)) {
         throw std::runtime_error(strprintf("ActivateBestChain failed. (%s)", FormatStateMessage(state)));
     }
 
     nScriptCheckThreads = 3;
-    for (int i = 0; i < nScriptCheckThreads - 1; i++)
+    std::cout << "nScriptCheckThreads\n";
+    for (int i = 0; i < nScriptCheckThreads - 1; i++){
         threadGroup.create_thread(&ThreadScriptCheck);
+    }
 
     g_banman = MakeUnique<BanMan>(GetDataDir() / "banlist.dat", nullptr, DEFAULT_MISBEHAVING_BANTIME);
     g_connman = MakeUnique<CConnman>(0x1337, 0x1337); // Deterministic randomness for tests.
+    std::cout << "TestingSetup finished\n";
 }
 
 TestingSetup::~TestingSetup()
