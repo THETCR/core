c0ee01b64dfd94b857774d34e4e0e8c86c21a112
TCR <thetcr@live.nl>
2019-04-03 15:56:20 +0200

    [CORE] Backport fixes

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -691,7 +691,7 @@ static std::string FormatException(const std::exception* pex, const char* pszThr
     char pszModule[MAX_PATH] = "";
     GetModuleFileNameA(nullptr, pszModule, sizeof(pszModule));
 #else
-    const char* pszModule = "WISPR";
+    const char* pszModule = "wispr";
 #endif
     if (pex)
         return strprintf(
@@ -710,13 +710,13 @@ void PrintExceptionContinue(const std::exception* pex, const char* pszThread)
 
 fs::path GetDefaultDataDir()
 {
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\WISPR
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\WISPR
-    // Mac: ~/Library/Application Support/WISPR
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Wispr
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Wispr
+    // Mac: ~/Library/Application Support/Wispr
     // Unix: ~/.wispr
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "WISPR";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Wispr";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -726,7 +726,7 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/WISPR";
+    return pathRet / "Library/Application Support/Wispr";
 #else
     // Unix
     return pathRet / ".wispr";
