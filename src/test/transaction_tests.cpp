a9940f62831ba979903535a161c53cf40e27872f
TCR <thetcr@live.nl>
2019-04-05 12:36:06 +0200

    [CORE] Backport fixes

--- src/test/transaction_tests.cpp
+++ src/test/transaction_tests.cpp
@@ -403,7 +403,7 @@ static void ReplaceRedeemScript(CScript& script, const CScript& redeemScript)
 BOOST_AUTO_TEST_CASE(test_big_witness_transaction)
 {
     CMutableTransaction mtx;
-    mtx.nVersion = 1;
+    mtx.nVersion = 2;
 
     CKey key;
     key.MakeNewKey(true); // Need to use compressed keys in segwit or the signing will fail
