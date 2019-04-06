a5adfc6c73ae8c81cf6f855754d324abb6cb8d14
TCR <thetcr@live.nl>
2019-04-06 23:26:28 +0200

    [CORE] Backport

--- src/miner.cpp
+++ src/miner.cpp
@@ -229,27 +229,27 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
     }
     pblock->nNonce         = 0;
     //Calculate the accumulator checkpoint only if the previous cached checkpoint need to be updated
-    uint256 nCheckpoint;
-    uint256 hashBlockLastAccumulated = chainActive[nHeight - (nHeight % 10) - 10]->GetBlockHash();
-    if (nHeight >= pCheckpointCache.first || pCheckpointCache.second.first != hashBlockLastAccumulated) {
-        //For the period before v2 activation, zWSP will be disabled and previous block's checkpoint is all that will be needed
-        pCheckpointCache.second.second = pindexPrev->nAccumulatorCheckpoint;
-        if (pindexPrev->nHeight + 1 >= Params().NEW_PROTOCOLS_STARTHEIGHT()) {
-            AccumulatorMap mapAccumulators(Params().Zerocoin_Params(false));
-            if (fZerocoinActive && !CalculateAccumulatorCheckpoint(nHeight, nCheckpoint, mapAccumulators)) {
-                LogPrintf("%s: failed to get accumulator checkpoint\n", __func__);
-            } else {
-                // the next time the accumulator checkpoint should be recalculated ( the next height that is multiple of 10)
-                pCheckpointCache.first = nHeight + (10 - (nHeight % 10));
-
-                // the block hash of the last block used in the accumulator checkpoint calc. This will handle reorg situations.
-                pCheckpointCache.second.first = hashBlockLastAccumulated;
-                pCheckpointCache.second.second = nCheckpoint;
-            }
-        }
-    }
-
-    pblock->nAccumulatorCheckpoint = pCheckpointCache.second.second;
+//    uint256 nCheckpoint;
+//    uint256 hashBlockLastAccumulated = chainActive[nHeight - (nHeight % 10) - 10]->GetBlockHash();
+//    if (nHeight >= pCheckpointCache.first || pCheckpointCache.second.first != hashBlockLastAccumulated) {
+//        //For the period before v2 activation, zWSP will be disabled and previous block's checkpoint is all that will be needed
+//        pCheckpointCache.second.second = pindexPrev->nAccumulatorCheckpoint;
+//        if (pindexPrev->nHeight + 1 >= Params().NEW_PROTOCOLS_STARTHEIGHT()) {
+//            AccumulatorMap mapAccumulators(Params().Zerocoin_Params(false));
+//            if (fZerocoinActive && !CalculateAccumulatorCheckpoint(nHeight, nCheckpoint, mapAccumulators)) {
+//                LogPrintf("%s: failed to get accumulator checkpoint\n", __func__);
+//            } else {
+//                // the next time the accumulator checkpoint should be recalculated ( the next height that is multiple of 10)
+//                pCheckpointCache.first = nHeight + (10 - (nHeight % 10));
+//
+//                // the block hash of the last block used in the accumulator checkpoint calc. This will handle reorg situations.
+//                pCheckpointCache.second.first = hashBlockLastAccumulated;
+//                pCheckpointCache.second.second = nCheckpoint;
+//            }
+//        }
+//    }
+//
+//    pblock->nAccumulatorCheckpoint = pCheckpointCache.second.second;
     pblocktemplate->vTxSigOpsCost[0] = WITNESS_SCALE_FACTOR * GetLegacySigOpCount(*pblock->vtx[0]);
 
     CValidationState state;
