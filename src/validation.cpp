3bc375c9233687c573623dafc682465ff417b566
TCR <thetcr@live.nl>
2019-04-07 01:27:14 +0200

    [CORE] Backport

--- src/validation.cpp
+++ src/validation.cpp
@@ -5372,7 +5372,7 @@ bool CChainState::AcceptBlock(const std::shared_ptr<const CBlock>& pblock, CVali
         return false;
 
     std::cout << "PivProtocolsStartHeightSmallerThen\n";
-    if(Params().PivProtocolsStartHeightSmallerThen(pindexPrev->nHeight + 1 && block.vtx.size() > 1)) {
+    if(pindexPrev && Params().PivProtocolsStartHeightSmallerThen(pindexPrev->nHeight + 1)) {
         std::cout << "CheckCoinStakeTimestamp\n";
         if (block.IsProofOfStake() && !CheckCoinStakeTimestamp(block.GetBlockTime(), (int64_t) block.vtx[1]->nTime)) {
             std::cout << "coinstake timestamp\n";
