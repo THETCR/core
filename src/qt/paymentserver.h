ba05913d3f545ccd257fd6f2598a6aece3d57f3d
TCR <thetcr@live.nl>
2019-04-08 14:12:28 +0200

    [CORE] Backport

--- src/qt/paymentserver.h
+++ src/qt/paymentserver.h
@@ -57,7 +57,7 @@ class QUrl;
 QT_END_NAMESPACE
 
 // BIP70 max payment request size in bytes (DoS protection)
-static const qint64 BIP70_MAX_PAYMENTREQUEST_SIZE = 50000;
+static const qint64 BIP70_MAX_PAYMENTREQUEST_SIZE = 6000;
 
 class PaymentServer : public QObject
 {
