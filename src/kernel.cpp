fbe83006b1c5ec09fb409e2a8f090be9323706cf
TCR <thetcr@live.nl>
2019-04-09 04:11:59 +0200

    [CORE] FIXES

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -440,7 +440,8 @@ bool CheckProofOfStake(const CBlock block, uint256& hashProofOfStake, std::uniqu
     } else {
         // First try finding the previous transaction in database
         uint256 hashBlock;
-        if (!GetTransaction(txin.prevout.hash, txPrev, Params().GetConsensus(), hashBlock))
+        CBlockIndex* pindex = LookupBlockIndex(block.GetHash());
+        if (!GetTransaction(txin.prevout.hash, txPrev, Params().GetConsensus(), hashBlock, pindex))
             return error("CheckProofOfStake() : INFO: read txPrev failed");
 
         //verify signature and script
